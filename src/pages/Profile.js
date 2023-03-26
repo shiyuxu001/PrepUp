a91000ba89459382c95ab3806b19c2d2a8552871
Dorothy <dorothynie@gmail.com>
2023-03-25 19:17:40 -0500

    Profile Page skeleton

---
+++ src/pages/Profile.js
@@ -0,0 +1,33 @@
+import '../styles/Profile.css';
+
+function Profile() {
+    return (
+        <div>
+            <button className="back-button">Back</button>
+            <div className="container">
+                <div className="profile-pic">
+
+                </div>
+            </div>
+            <h1 className="profile-name">John Doe</h1>
+            <div className="container">
+                <div className="profile-setting">
+                    <p className="setting-header">Experience level</p>
+                    <div className="setting">
+
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="profile-setting">
+                    <p className="setting-header">Experience level</p>
+                    <div className="setting">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
