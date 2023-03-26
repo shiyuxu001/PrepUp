1d86ea19a4401196622a8c84eecebd68f3cfe483
Dorothy <dorothynie@gmail.com>
2023-03-25 19:18:38 -0500

    Reset index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Profile />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
